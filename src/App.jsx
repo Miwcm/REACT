833a1d013dd44add315dc1349f6e69029c34f208
Miwa Martinez <miwa.mtz@gmail.com>
2025-10-20 17:05:02 -0300

    Creo carpeta constants y datos-parrafos para agregarlos al App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@
 import { Fragment } from "react"
 import TituloPrincipal from "./components/TituloPrincipal"
 import TituloSecundario from "./components/TituloSecundario"
+import Parrafo from "./components/Parrafo"
+import datosParrafos from "./constants/datos-parrafos"
 
 // handle-http.js <-- modulo de js
 // App.jsx <--- Si trabajo con componentes, utilizo mayusculas
@@ -22,13 +24,26 @@ const App = () => {
   // * Lo que retornan las funciones (componentes) no es HTML es JSX -> Javascript + XML (Extension de JS) <--- Facebook(Meta)
   // * JSX -> Fragment -> <></>
   // * JSX -> Fragment -> <Fragment></Fragment>
+
+
+
   return (
       <>
-        <TituloPrincipal /> {/*Cierre en la etiqueta de apertura */}
-        <TituloSecundario texto="Titulo Secundario" nro="1" color="crimson"></TituloSecundario>{/*etiqueta apertura y etiqueta cierre */}
-        <TituloSecundario texto="Titulo Secundario" nro="2" color="lightblue"/>
-        <TituloSecundario texto="Titulo Secundario" nro="3" color="violet"/>
-        <TituloSecundario texto="Titulo Secundario" nro="4" color="lightgreen"/>
+        <TituloPrincipal /> {/* Cierre en la etiqueta de apertura */}
+        
+        <TituloSecundario texto="Título Secundario" nro="1" color="crimson"></TituloSecundario> {/* Etiqueta apertura y etiqueta cierre */}
+
+        <Parrafo texto={datosParrafos[0].texto} numero={datosParrafos[0].numero} />
+
+        <TituloSecundario texto="Titulo Secundario" nro="2" color="violet" /> 
+
+        <Parrafo texto={datosParrafos[1].texto} numero={datosParrafos[1].numero} />
+
+        <TituloSecundario texto="Titulo Secundario" nro="3" color="lightblue" />
+
+        <Parrafo texto={datosParrafos[2].texto} numero={datosParrafos[2].numero} />
+
+        <TituloSecundario texto="Titulo Secundario" nro="4" color="lightgreen" />
       </>
     )
 }
---
+++ src/components/Parrafo.jsx
@@ -0,0 +1,9 @@
+
+const Parrafo = (props) => {
+    console.log(props); // {numero}
+  return (
+    <div>Parrafo</div>
+  )
+}
+
+export default Parrafo
